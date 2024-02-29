621fe1b5f7da31cc229816be5978fd462192e703
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-02-29 20:59:32 +0530

    class name err fixed

--- src/components/Weathercard.jsx
+++ src/components/Weathercard.jsx
@@ -4,25 +4,25 @@ const Weathercard = ({cityWeather}) => {
   return (
     <div style={{display: "flex", justifyContent: "center"}}>
         <div
-      className="weather-card"
+      className="weather-cards"
     >
       <h3>Temperature</h3>
       <p>{cityWeather["temp_c"]}&deg;C</p>
     </div>
     <div
-    className="weather-card"
+    className="weather-cards"
     >
       <h3>Humidity</h3>
       <p>{cityWeather.humidity}%</p>
     </div>
     <div
-      className="weather-card"
+      className="weather-cards"
     >
       <h3>Condition</h3>
       <p>{cityWeather.condition.text}</p>
     </div>
     <div
-      className="weather-card"
+      className="weather-cards"
     >
       <h3>Wind Speed</h3>
       <p>{cityWeather["wind_kph"]}kph</p>
